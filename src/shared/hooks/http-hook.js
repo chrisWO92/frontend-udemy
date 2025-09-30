fd573e01871d0bf981ae8a763da67e984b13aabd
chriswo92 <chrisworkingonline@gmail.com>
2025-09-30 07:09:57 -0300

    http-hook 1

---
+++ src/shared/hooks/http-hook.js
@@ -0,0 +1,31 @@
+import { useState } from "react";
+
+export const useHttpCLient = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState();
+
+  const sendRequest = async (
+    url,
+    method = "GET",
+    body = null,
+    headers = {}
+  ) => {
+    setIsLoading(true);
+    try {
+      const response = await fetch(url, {
+        method,
+        body,
+        headers,
+      });
+
+      const responseData = await response.json();
+
+      if (!response.ok) {
+        throw new Error(responseData.message);
+      }
+    } catch (err) {
+      setError(err.message);
+    }
+    setIsLoading(false);
+  };
+};
