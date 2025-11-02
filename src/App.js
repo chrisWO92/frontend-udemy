d797b9b1539cf7994b52c7d188b41a464adc510f
chriswo92 <chrisworkingonline@gmail.com>
2025-11-02 11:39:09 -0300

    auth logic finished

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import React, { useState, useCallback, useEffect } from "react";
+import React from "react";
 import {
   BrowserRouter as Router,
   Route,
@@ -13,51 +13,13 @@ import UserPlaces from "./places/pages/UserPlaces";
 import UpdatePlace from "./places/pages/UpdatePlace";
 import Auth from "./user/pages/Auth";
 import { AuthContext } from "./shared/context/auth-context";
+import { useAuth } from "./shared/hooks/auth-hook";
 
 // Este es el segundo nivel de nuestra aplicación, el componente principal App
 const App = () => {
-  // el siguiente estado se usa para determinar si el usuario está logeado o no
-  const [token, setToken] = useState(false);
-  const [userId, setuserId] = useState(false);
-
-  const login = useCallback((uid, token, expirationDate) => {
-    setToken(token);
-    setuserId(uid);
-    const tokenExpirationDate =
-      expirationDate || new Date(new Date().getTime() + 1000 * 60 * 60);
-    localStorage.setItem(
-      "userData",
-      JSON.stringify({
-        userId: uid,
-        token: token,
-        expiration: tokenExpirationDate.toISOString(),
-      })
-    );
-  }, []);
-
-  const logout = useCallback(() => {
-    setToken(null);
-    setuserId(null);
-    localStorage.removeItem("userData");
-  }, []);
-
-  useEffect(() => {
-    const storedData = JSON.parse(localStorage.getItem("userData"));
-    if (
-      storedData &&
-      storedData.token &&
-      new Date(storedData.expiration) > new Date()
-    ) {
-      login(
-        storedData.userId,
-        storedData.token,
-        new Date(storedData.expiration)
-      );
-    }
-  }, [login]);
+  const { token, login, logout, userId } = useAuth();
 
   let routes;
-  console.log(token);
 
   // definimos las rutas que se renderizan si estamos logeados o no
   // Switch sirve para que el enrutador renderice sólo una de las rutas cuando encuentre la primera coincidencia, ignorando el resto. Esto es útil porque si no se pone Switch, React Router evaluaría todas las rutas que coincidan con la URL y las renderizaría todas juntas.
